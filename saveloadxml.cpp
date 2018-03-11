690d23e772fea107baccf565a12ba0a9eb30376d
Tomasz Bieniek <BuxBleed@gmail.com>
2018-03-11 15:45:25 +0100

    Update 11.03.2018

---
+++ saveloadxml.cpp
@@ -0,0 +1,6 @@
+#include "saveloadxml.h"
+
+SaveLoadXML::SaveLoadXML()
+{
+
+}
---
+++ saveloadxml.h
@@ -0,0 +1,11 @@
+#ifndef SAVELOADXML_H
+#define SAVELOADXML_H
+
+
+class SaveLoadXML
+{
+public:
+    SaveLoadXML();
+};
+
+#endif // SAVELOADXML_H
